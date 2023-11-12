3ac5977979dec936728e438c5cd240fc8029cbe0
MARIEM0123 <M00riemhissou@gmail.com>
2023-11-12 13:33:29 +0000

    Airbnb

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,5 @@
 """__init__ initialization for python models and directory"""
 from models.engine.file_storage import FileStorage
 
-stg = FileStorage()
-stg.reload()
+storage = FileStorage()
+storage.reload()
